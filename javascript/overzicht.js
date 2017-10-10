a6cc13be9ab024597e447320667f0c573e990df0
SummerDanoe <31731680+SummerDanoe@users.noreply.github.com>
2017-10-10 14:24:35 +0200

    heel veel aanpassingen

---
+++ javascript/overzicht.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+//bron:https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
