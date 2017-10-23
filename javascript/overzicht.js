43d992cf4326851716c6aca39cd219ec388c3f90
SummerDanoe <31731680+SummerDanoe@users.noreply.github.com>
2017-10-23 15:00:08 +0200

    zoveel veranderd
    Er is zoveel veranderd omg

--- javascript/overzicht.js
+++ javascript/overzicht.js
@@ -1,16 +1,8 @@
-// JavaScript Document
-//bron:https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
     }
 }
