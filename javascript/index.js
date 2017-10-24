fa4a9327a8528245163304509a4bd72790e99b8b
SummerDanoe <31731680+SummerDanoe@users.noreply.github.com>
2017-10-24 22:42:54 +0200

    werkt voor geen kut
    althans dat denk ik dan. misschien ben ik blind, maar ik ga nu proberen het semi te fixen

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,6 @@
 // JavaScript Document
+
+//dropdown
 document.getElementById('menuButton').onclick = function() { 
     var iPadMenu = document.getElementById("iPadMenu");
 
@@ -14,3 +16,20 @@ document.getElementById('menuButton').onclick = function() {
     }
 
 };
+//kaartjes in en uit klap
+// bron: Sanne 't Hooft project 1
+(function() {
+	
+	var cmdaCards = document.querySelectorAll("section");
+	for (var i=0;i<cmdaCards.length;i++) {
+		
+		var handvat = document.createElement("span");
+		handvat.classList.add("card-handvat"); 
+		cmdaCards[i].appendChild(handvat);
+		
+		handvat = cmdaCards[i].querySelector(".card-handvat");
+		handvat.addEventListener("click", function(event){
+			this.parentNode.classList.toggle("active");
+		});
+	}
+})();
