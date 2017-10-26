1655d9923934f70c912208029ab2d89184b74d6c
SummerDanoe <31731680+SummerDanoe@users.noreply.github.com>
2017-10-26 13:50:53 +0200

    Het werkt
    Het heeft even geduurd, maar met wat vergezochte javascript en de hulp van een  stackoverflow tovenaar die waarschijnlijk een magische staf heeft en in een grot leeft, werkt het eindelijk.
    de header moet nog een beetje beter worden, maar het komt goed.

--- javascript/index.js
+++ javascript/index.js
@@ -18,19 +18,25 @@ document.getElementById('menuButton').onclick = function() {
 };
 //kaartjes in en uit klap
 // bron: Jhttps://codepen.io/joostf/pen/bwrryR
-
-
-var section = document.querySelector('section > summary > p');
-var button = document.querySelector('section > summary > button');
+/*
+var section = document.querySelectorAll('section > summary > p');
+var button = document.querySelectorAll('section > summary > button');
 
 var show = function () {
 	section.classList.toggle('show')
 }
 
-button.addEventListener('click', show);
+button.addEventListener('click', show());*/
 
 
+// Collect all buttons, and store them in an Array using querySelectorAll
+var buttons = document.querySelectorAll('button')
 
-
-
-
+// Initiate a for loop that 'loops' through all options in the buttons array.
+for (var i = 0; i < buttons.length; i++) {
+  // For every instance of buttons in the Array, apply an event listener (click).
+  buttons[i].addEventListener('click', function(e) {
+     // Grab the target of the click event, select their parent and access the 7th child (the <p> tag in question). Access the classList and toggle the 'show' class.
+     e.target.parentNode.childNodes[7].classList.toggle('show');
+  });
+};
