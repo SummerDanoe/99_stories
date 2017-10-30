3e360130c3909113163823c0de67147fdc147d64
SummerDanoe <31731680+SummerDanoe@users.noreply.github.com>
2017-10-30 21:19:37 +0100

    beter zo
    dat ongelovelijke kutding van een javascript toggle heb ik eruit gehaald. Ik heb liever dat het er normaal uit ziet, dan dat het half werkt.

--- javascript/index.js
+++ javascript/index.js
@@ -1,30 +1,12 @@
-// JavaScript Document
+//Javascript Document
+var nav = document.querySelector('header > nav');
+var button = document.querySelector('header > button');
 
-//dropdown
-document.getElementById('menuButton').onclick = function() { 
-    var iPadMenu = document.getElementById("iPadMenu");
+var open = function () {
 
-    if (iPadMenu.style.display != "block") {
-        iPadMenu.style.display = "block";
+	nav.classList.toggle('open')
+}
 
-        styles = '#menuButton:after { display: block; }';
-    }
-    else {
-        iPadMenu.style.display = "none";
+button.addEventListener('click', open);
 
-        styles = '#menuButton:after { display: none; }';
-    }
 
-};
-
-// Collect all buttons, and store them in an Array using querySelectorAll
-var buttons = document.querySelectorAll('button')
-
-// Initiate a for loop that 'loops' through all options in the buttons array.
-for (var i = 0; i < buttons.length; i++) {
-  // For every instance of buttons in the Array, apply an event listener (click).
-  buttons[i].addEventListener('click', function(e) {
-     // Grab the target of the click event, select their parent and access the 7th child (the <p> tag in question). Access the classList and toggle the 'show' class.
-     e.target.parentNode.childNodes[7].classList.toggle('show'); 
-  });
-};
